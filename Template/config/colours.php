b4696b4d010ba4dfe026aef1cbfbf74eb0d96e2e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-20 02:45:55 +0000

    Add count

--- Template/config/colours.php
+++ Template/config/colours.php
@@ -1,29 +1,47 @@
+<?php $defaultColours = $this->task->colorModel->getDefaultColors(); ?>
+
 <div class="page-header">
     <h2><?= t('Available Colours') ?></h2>
 </div>
 
-<div class="">
-    <h4>default colour: <span><?= $this->task->colorModel->getDefaultColor(); ?></span></h4>
+<div class="default-colour">
+    <ul>
+        <li>
+            default colour: <span><?= $this->task->colorModel->getDefaultColor(); ?></span>
+        </li>
+        <li>
+            <?= count($defaultColours) ?>
+        </li>
+    </ul>
 </div>
 
-<?php $defaultColours = $this->task->colorModel->getDefaultColors(); ?>
 
-<table>
+<div class=""></div>
+
+
+
+
+
+<table id="" class="colour-table">
     <thead>
-        <tr>
-            <th>Colour</th>
-            <th>CSS Background</th>
-            <th>CSS Border</th>
+        <tr class="">
+            <th class="">Colour</th>
+            <th class="">CSS Background</th>
+            <th class="">CSS Border</th>
+            <th class="">Example</th>
         </tr>
     </thead>
     <tbody>
-<?php foreach ($defaultColours as $color_id => $color): ?>
-        <tr>
-            <td><?= $color['name'] ?></td>
-            <td><?= $color['background'] ?></td>
-            <td><?= $color['border'] ?></td>
+        <?php foreach ($defaultColours as $color_id => $color): ?>
+        <tr class="">
+            <td class=""><?= $color['name'] ?></td>
+            <td class=""><?= $color['background'] ?></td>
+            <td class=""><?= $color['border'] ?></td>
+            <td class="">
+                <span class="colour-example"><?= $color['name'] ?></span>
+            </td>
         </tr>
-<?php endforeach ?>
+        <?php endforeach ?>
     </tbody>
 </table>
 
