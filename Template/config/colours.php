a52974f1099f0e00b74f4aef3f352a4e5a92ece9
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-20 02:36:53 +0000

    Add basic colour table

--- Template/config/colours.php
+++ Template/config/colours.php
@@ -6,3 +6,24 @@
     <h4>default colour: <span><?= $this->task->colorModel->getDefaultColor(); ?></span></h4>
 </div>
 
+<?php $defaultColours = $this->task->colorModel->getDefaultColors(); ?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Colour</th>
+            <th>CSS Background</th>
+            <th>CSS Border</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php foreach ($defaultColours as $color_id => $color): ?>
+        <tr>
+            <td><?= $color['name'] ?></td>
+            <td><?= $color['background'] ?></td>
+            <td><?= $color['border'] ?></td>
+        </tr>
+<?php endforeach ?>
+    </tbody>
+</table>
+
