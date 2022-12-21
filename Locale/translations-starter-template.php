332b404b1ba363808ebde781a20dfc94b8e887f3
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-21 04:53:39 +0000

    Add Translations

--- Locale/translations-starter-template.php
+++ Locale/translations-starter-template.php
@@ -3,4 +3,17 @@ return array(
   //
   // GENERAL
   //
+  'This plugin shows the actual colour values (background and matching border) for all the default Kanboard colours in the application settings.' => '',
+  //
+  // KBColoursContoller.php
+  //
+  'Default Colours' => '',
+  //
+  // config/colours.php
+  //
+  'Available Colours' => '',
+  'Default Colour' => '',
+  'Total Colours' => '',
+  'Background Colour' => '',
+  'Border Colour' => '',
 );
