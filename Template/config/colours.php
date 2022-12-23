a47175bad88de4f2d5bb2cc5f11e469356bc5416
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-23 22:49:29 +0000

    Change Colour Term
    WIP #2

--- Template/config/colours.php
+++ Template/config/colours.php
@@ -10,22 +10,22 @@
         <svg width="22" height="22" fill="currentColor" class="bi bi-palette-fill" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
         </svg>
-        <?= t('Available Colours') ?> <span class="total-count"><?= count($this->task->colorModel->getList()) ?></span>
+        <?= t('Available Colors') ?> <span class="total-count"><?= count($this->task->colorModel->getList()) ?></span>
     </h2>
     <hr>
     <div class="kbc-section">
-        <h3>Default Colour Palette<span class="total-count"><?= count($defaultColours); ?></span></h3>
+        <h3><?= t('Default Color Palette') ?><span class="total-count"><?= count($defaultColours); ?></span></h3>
     </div>
     <div class="colour-section">
         <ul class="">
             <li class="">
-                <strong><?= t('Default Task Colour') ?>:</strong>
+                <strong><?= t('Default Task Color') ?>:</strong>
                 <span class="default-box-colour" style="background-color: <?= $defaultBackground ?>; border-color: <?= $defaultBorder ?>;">
                     <?= ucfirst($this->task->colorModel->getDefaultColor()); ?>
                 </span>
             </li>
             <li class="note">
-                <?= t('Change the default colour in') ?> <?= $this->url->link(t('Project Settings'), 'ConfigController', 'project', array(), false, 'note-link', t('Go to Project Settings')) ?>
+                <?= t('Change the default color in') ?> <?= $this->url->link(t('Project Settings'), 'ConfigController', 'project', array(), false, 'note-link', t('Go to Project Settings')) ?>
             </li>
         </ul>
     </div>
@@ -36,7 +36,7 @@
             <div class="colour-wrapper" style="background-color: <?= $color['background'] ?>; border-color: <?= $color['border'] ?>;" title="<?= $color['name'] ?>">
                 <div class="colour-name"><?= $color['name'] ?></div>
                 <div class="colour-background">
-                    <span class=""><?= t('Background Colour') ?></span>
+                    <span class=""><?= t('Background Color') ?></span>
                     <code class="">
                         <?php if (strpos($color['background'], '#') !== false): ?>
                             <?= strtoupper($color['background']) ?>
@@ -46,7 +46,7 @@
                     </code>
                 </div>
                 <div class="colour-border">
-                    <span class=""><?= t('Border Colour') ?></span>
+                    <span class=""><?= t('Border Color') ?></span>
                     <code class="">
                         <?php if (strpos($color['border'], '#') !== false): ?>
                             <?= strtoupper($color['border']) ?>
@@ -60,11 +60,11 @@
     </div>
 
     <div class="kbc-section">
-        <h3 class="">Extra Colour Palette<span class="total-count">00</span></h3>
+        <h3 class=""><?= t('Extra Color Palette') ?><span class="total-count">00</span></h3>
     </div>
 
     <div class="kbc-section">
-        <h3 class="">Custom Colour Palette<span class="total-count"><?= $customColours; ?></span></h3>
+        <h3 class=""><?= t('Custom Color Palette') ?><span class="total-count"><?= $customColours; ?></span></h3>
     </div>
     
     <button><?= $this->modal->small('plus', t('Add Color'), 'KBColoursController', 'add', ['plugin' => 'kBColours'], false, 'popover') ?></button>
@@ -76,11 +76,11 @@
                 <div class="colour-wrapper" style="background-color: <?= $color['background'] ?>; border-color: <?= $color['border'] ?>;" title="<?= $color['name'] ?>">
                     <div class="colour-name"><?= $color['name'] ?>
                         <span class="" style="float:right">
-                            <?= $this->url->icon('trash', '', 'KBColoursController', 'remove', ['plugin' => 'kBColours', 'key' => $color_id]) ?>
+                            <?= $this->url->icon('trash', '', 'KBColoursController', 'remove', ['plugin' => 'kBColours', 'key' => $color_id], false, 'delete-icon', t('Delete Color')) ?>
                         </span>
                     </div>
                     <div class="colour-background">
-                        <span class=""><?= t('Background Colour') ?></span>
+                        <span class=""><?= t('Background Color') ?></span>
                         <code class="">
                             <?php if (strpos($color['background'], '#') !== false): ?>
                                 <?= strtoupper($color['background']) ?>
@@ -90,7 +90,7 @@
                         </code>
                     </div>
                     <div class="colour-border">
-                        <span class=""><?= t('Border Colour') ?></span>
+                        <span class=""><?= t('Border Color') ?></span>
                         <code class="">
                             <?php if (strpos($color['border'], '#') !== false): ?>
                                 <?= strtoupper($color['border']) ?>
