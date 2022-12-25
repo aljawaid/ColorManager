465ffe664a008cc0885dcb760fd82916e92b7fd0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-25 16:42:37 +0000

    Fix Translation String Order
    #2

--- Locale/translations-starter-template.php
+++ Locale/translations-starter-template.php
@@ -29,10 +29,10 @@ return array(
   'Project Settings' => '',
   'Go to Project Settings' => '',
   'Extra Color Palette' => '',
-  'Custom Color Palette' => '',
-  'Delete Color' => '',
   'This palette shows a collection of colors different to those in the default palette. Selected colors also use alternative colors for text. These colors are particularly suitable for tags and categories.' => '',
   'Colors are named based on their common color name. If text colors are used, then the names are preceded by their text color.' => '',
+  'Custom Color Palette' => '',
   'This palette shows all user created colors. These colors are unique to your application.' => '',
   'Color names matching colors in other palettes will override those colors. Duplicate color names will not be created.' => '',
+  'Delete Color' => '',
 );
