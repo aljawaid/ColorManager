ea43ca4e6d942246a7265b61fec7c4f6ad9c346a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-27 01:44:33 +0000

    Change default modal color

--- Template/config/add_custom_color.php
+++ Template/config/add_custom_color.php
@@ -7,7 +7,7 @@
     <?= $this->form->text('color_name', $values, $errors, ['required']) ?>
     
     <label for="background_color"><?= t('Background Color') ?></label>
-    <input type="color" id="background_color" name="background_color" value="#0000ff">
+    <input type="color" id="background_color" name="background_color" value="#FF7800">
     <label for="border_color"><?= t('Border Color') ?></label>
     <input type="color" id="border_color" name="border_color" value="#000000">
     
