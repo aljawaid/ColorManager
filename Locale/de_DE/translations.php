3bb49606f94d6d31ffa13e3741ee9c7eccbad200
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-31 01:32:41 +0000

    FIX: Translations Coding Consistency
    #21

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -1,30 +1,40 @@
 <?php
 
 return array(
-'A new way to utilise colors to their full potential in Kanboard. Choose from 3 color palettes, default, extra (includes white) or create your own. Across all palettes, choose from over 50 colors to match your workflow across the interface.'  =>  'Eine neue Möglichkeit um Farben in Kanboard nutzen zu können. Es gibt jetzt drei Farbpaletten. Die Standard Palette, eine erweiterte (Extra) Palette inkl. der Farbe Weiß sowie eine eigene Palette. Bereits in der Grundausstattung stehen damit 50 Farben für alle Bereiche (Tasks, Tags usw.) zur Verfügung.',
-'Settings'  =>  'Einstellungen',
-'Color Manager'  =>  'Farbmanagement',
-'color-manager'  =>  'Farbmanagement',
-'Custom Colors'  =>  'Eigene Farben',
-'Add Color'  =>  'Farbe hinzufügen',
-'Color Name'  =>  'Name der Farbe',
-'Background Color'  =>  'Hintergrundfarbe',
-'Border Color'  =>  'Rahmenfarbe',
-'Save Color'  =>  'Farbe speichern',
-'Available Colors'  =>  'Verfügbare Farben',
-'Default Color Palette'  =>  'Standard Farbpalette',
-'These colors are part of the core. Pastel styles make these colors universal across all features of the application.'  =>  'Diese Farben sind Bestandteil des Kanboard Cores. Pasteltöne sorgen dafür, dass diese universell für alle Bereiche der Applikation verwendet werden können.',
-'Task Color'  =>  'Task Farbe',
-'Change the default color in'  =>  'Ändere die Standardfarbe zu',
-'Project Settings'  =>  'Projekteinstellungen',
-'Go to Project Settings'  =>  'Geh zu den Projekteinstellungen',
-'Extra Color Palette'  =>  'Extra Farbpalette',
-'This palette shows a collection of colors different to those in the default palette. Selected colors also use alternative colors for text. These colors are particularly suitable for tags and categories.'  =>  'Diese Palette enthält eine Auswahl von Farben die sich von denen der Standard Palette unterscheiden. Einige Farben bringen auch eine andere Textfarbe mit, damit diese auch für Kategorien und Tags einfach verwendbar sind.',
-'Colors are named based on their common color name. If text colors are used, then the names are preceded by their text color.'  =>  'Die Farben werden auf der Grundlage ihres allgemeinen Farbnamens benannt. Wenn Textfarben verwendet werden, wird den Namen die Textfarbe vorangestellt.',
-'Add to this palette'  =>  'Zu dieser Palette hinzufügen',
-'Custom Color Palette'  =>  'Benutzerdefinierte Farbpalette',
-'This palette shows all user created colors. These colors are unique to your application.'  =>  'Diese Palette zeigt alle vom Benutzer erstellten Farben. Diese Farben sind individuell für Ihre Anwendung.',
-'Color names matching colors in other palettes will override those colors. Duplicate color names will not be created.'  =>  'Farbnamen, die mit Farben in anderen Paletten übereinstimmen, setzen diese Farben außer Kraft. Doppelte Farbnamen werden nicht erstellt.',
-'Delete Color'  =>  'Farbe löschen',
-'Amber'  =>  'Bernstein',
+  //
+  // GENERAL
+  //
+  'A new way to utilise colors to their full potential in Kanboard. Choose from 3 color palettes, default, extra (includes white) or create your own. Across all palettes, choose from over 50 colors to match your workflow across the interface.'  =>  'Eine neue Möglichkeit um Farben in Kanboard nutzen zu können. Es gibt jetzt drei Farbpaletten. Die Standard Palette, eine erweiterte (Extra) Palette inkl. der Farbe Weiß sowie eine eigene Palette. Bereits in der Grundausstattung stehen damit 50 Farben für alle Bereiche (Tasks, Tags usw.) zur Verfügung.',
+  //
+  // ColorManagerContoller.php
+  //
+  'Settings'  =>  'Einstellungen',
+  'Color Manager'  =>  'Farbmanagement',
+  'Custom Colors'  =>  'Eigene Farben',
+  'Add Color'  =>  'Farbe hinzufügen',
+  //
+  // config/add_custom_color.php
+  //
+  'Color Name'  =>  'Name der Farbe',
+  'Background Color'  =>  'Hintergrundfarbe',
+  'Border Color'  =>  'Rahmenfarbe',
+  'Save Color'  =>  'Farbe speichern',
+  //
+  // config/colors.php
+  //
+  'Available Colors'  =>  'Verfügbare Farben',
+  'Default Color Palette'  =>  'Standard Farbpalette',
+  'These colors are part of the core. Pastel styles make these colors universal across all features of the application.'  =>  'Diese Farben sind Bestandteil des Kanboard Cores. Pasteltöne sorgen dafür, dass diese universell für alle Bereiche der Applikation verwendet werden können.',
+  'Task Color'  =>  'Task Farbe',
+  'Change the default color in'  =>  'Ändere die Standardfarbe zu',
+  'Project Settings'  =>  'Projekteinstellungen',
+  'Go to Project Settings'  =>  'Geh zu den Projekteinstellungen',
+  'Extra Color Palette'  =>  'Extra Farbpalette',
+  'This palette shows a collection of colors different to those in the default palette. Selected colors also use alternative colors for text. These colors are particularly suitable for tags and categories.'  =>  'Diese Palette enthält eine Auswahl von Farben die sich von denen der Standard Palette unterscheiden. Einige Farben bringen auch eine andere Textfarbe mit, damit diese auch für Kategorien und Tags einfach verwendbar sind.',
+  'Colors are named based on their common color name. If text colors are used, then the names are preceded by their text color.'  =>  'Die Farben werden auf der Grundlage ihres allgemeinen Farbnamens benannt. Wenn Textfarben verwendet werden, wird den Namen die Textfarbe vorangestellt.',
+  'Add to this palette'  =>  'Zu dieser Palette hinzufügen',
+  'Custom Color Palette'  =>  'Benutzerdefinierte Farbpalette',
+  'This palette shows all user created colors. These colors are unique to your application.'  =>  'Diese Palette zeigt alle vom Benutzer erstellten Farben. Diese Farben sind individuell für Ihre Anwendung.',
+  'Color names matching colors in other palettes will override those colors. Duplicate color names will not be created.'  =>  'Farbnamen, die mit Farben in anderen Paletten übereinstimmen, setzen diese Farben außer Kraft. Doppelte Farbnamen werden nicht erstellt.',
+  'Delete Color'  =>  'Farbe löschen',
 );
