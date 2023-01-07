38c5006788cf05f8ab957ee2e7d4cf0058c1a9f1
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-07 20:23:27 +0000

    Add Styling for `text_colors`
    - WIP #29

--- Model/ColorModelExt.php
+++ Model/ColorModelExt.php
@@ -411,6 +411,28 @@ class ColorModelExt extends ColorModel
                 $buffer .= '}';
             }
         }
+        foreach ($this->text_colors as $color => $values) {
+            $buffer .= '.task-board.color-'.$color.', .task-summary-container.color-'.$color.', .color-picker-square.color-'.$color.', .task-board-category.color-'.$color.', .table-list-category.color-'.$color.', .task-tag.color-'.$color.' {';
+            $buffer .= 'background-color: '.$values['background'].';';
+            $buffer .= 'border-color: '.$values['border'].';';
+            if (isset($values['font'])) {
+                $buffer .= 'color: '.$values['font'].'; position: relative; }';
+                $buffer .= 'select#form-default_color option[value="'.$color.'"], option[value="'.$color.'"] {';
+                $buffer .= 'color: '.$values['font'].';';
+            }
+            $buffer .= '}';
+            if (isset($values['font'])) {
+                $buffer .= '.color-picker-square.color-'.$color.':before { content: "\2609"; text-align: center; display: block; position: absolute; margin-top: -4px; margin-left: 1px; }';
+                $buffer .= '.task-summary-container.color-'.$color.' .task-summary-columns .task-summary-column { color: '.$values['font'].'; }';
+                $buffer .= '.task-summary-container.color-'.$color.' .task-summary-columns .task-summary-column span { color: '.$values['font'].'; }';
+            }
+            $buffer .= 'td.color-'.$color.' { background-color: '.$values['background'].'; }';
+            $buffer .= '.table-list-row.color-'.$color.' { border-left: 5px solid '.$values['border'].'; }';
+            $buffer .= 'select#form-default_color option[value="'.$color.'"], option[value="'.$color.'"] {';
+            $buffer .= 'background-color: '.$values['background'].';';
+            $buffer .= 'border-color: '.$values['border'].';';
+            $buffer .= '}';
+        }
         foreach ($this->static_colors as $color => $values) {
             $buffer .= '.task-board.color-'.$color.', .task-summary-container.color-'.$color.', .color-picker-square.color-'.$color.', .task-board-category.color-'.$color.', .table-list-category.color-'.$color.', .task-tag.color-'.$color.' {';
             $buffer .= 'background-color: '.$values['background'].';';
