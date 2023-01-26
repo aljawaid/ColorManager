b5aa06c143df99ec8f248c55df9fcf1fa70e28d3
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-26 21:00:50 +0000

    FIX: Unix (LF) Line Endings and Tabs > Spaces

--- Plugin.php
+++ Plugin.php
@@ -20,7 +20,7 @@ class Plugin extends Base
         $this->template->hook->attach('template:layout:bottom', 'colorManager:layout/css_ext');
 
         // Extra Page - Routes
-        //  - Example: $this->route->addRoute('/my/custom/route', 'myController', 'show', 'PluginNameExampleStudlyCaps');
+        //  - Example: $this->route->addRoute('/my/custom/route', 'MyController', 'show', 'PluginNameExampleStudlyCaps');
         //  - Must have the corresponding action in the matching controller
         $this->route->addRoute('/settings/color-manager', 'ColorManagerController', 'show', 'ColorManager');
 
